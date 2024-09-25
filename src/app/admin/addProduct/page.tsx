8110d84d06b85c8aa4abf06f6bb5ec9dba883450
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-09-25 13:32:36 -0700

    button

--- src/app/admin/addProduct/page.tsx
+++ src/app/admin/addProduct/page.tsx
@@ -20,7 +20,9 @@ const Page = () => {
       <input className="w-full sm:w-[500px] mt-4 px-4 py-3 border" type="text" placeholder="Blog Title Here" required />
       
       <p className="text-xl mt-4">Blog Description</p>
-      <textarea className="w-full sm:w-[600px] mt-4 px-4 py-3 border"  placeholder="Blog Desciption Here" rows={8} required />      
+      <textarea className="w-full sm:w-[600px] mt-4 px-4 py-3 border"  placeholder="Blog Desciption Here" rows={8} required />  
+      <br />
+      <button type="submit" className="mt-8 w-40 h-12 bg-[#0066cc] text-white">ADD</button>   
       </form>
       
       
