4d5ac0803d34b1b61e97905d5b893e6b8585bb60
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-09-25 13:24:44 -0700

    admin

--- src/app/admin/addProduct/page.tsx
+++ src/app/admin/addProduct/page.tsx
@@ -11,10 +11,17 @@ const Page = () => {
     <form className="pt-5 px-5 sm:pt-12 sm:pl-16">
       <p className="text-xl ">Upload thumbnail</p>
       <label htmlFor="image">
-        <Image src={!image?"/upload_area.png":URL.createObjectURL(image)}  width={170} height={70} alt="Upload" className="mt-4"/>
+        <Image src={!image?"/upload_area.png":URL.createObjectURL(image)}  width={170} height={70} alt="Upload" className="mt-4 w-[170px] h-[100px]"
+        
+        />
       </label>
       <input onChange={(e)=>setImage(e.target.files ? e.target.files[0]:image)} type="file" id="image" hidden required />
-    </form>
+      <p className="text-xl mt-4">Blog Title</p>
+      <input className="w-full sm:w-[500px] mt-4 px-4 py-3 border" type="text" placeholder="Blog Title Here" required />
+      
+      <p className="text-xl mt-4">Blog Description</p>
+      <textarea className="w-full sm:w-[600px] mt-4 px-4 py-3 border"  placeholder="Blog Desciption Here" rows={8} required />      
+      </form>
       
       
        </>
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children, }: Readonly<{ children: React.ReactNo
     <div className="flex">
         <Sidebar />
         <div className="flex flex-col w-full">
-         <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+         <div className="flex items-center justify-between w-full py-3 max-h-[60px] md:px-12 px-6 border-b border-black">
            <h3 className="font-bold text-lg text-[#0066cc]">Admin Panel</h3>
            <Image src={"/admin.webp"} width={40} height={40} alt="Admin" className="rounded-full"/>
          </div>
