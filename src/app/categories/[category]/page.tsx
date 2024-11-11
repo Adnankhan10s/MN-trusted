6d82421a2cb8cddfbc743e29968f4af7327feef7
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-11-11 13:17:09 -0800

    add services

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import { categories } from '../../../../serviceData';
 import { notFound } from 'next/navigation';
 import { Oswald } from 'next/font/google';
-import CatComponent from '@/components/otherComponents/CatComponent';
+import ItemList from '@/components/otherComponents/ItemList';
 
 const oswald = Oswald({
    weight:"600",
@@ -29,18 +29,25 @@ const page = ({params}:CategoryPageProps) => {
 
     return (
         
-        <div className='w-full h-[1500px] bg-slate-200 '>
+        <div className='w-full h-full  bg-slate-200 '>
              <Navbar />
-             <div className='w-full h-[300px] md:h-[500px] shadow-md pt-20 '>
+             <div className='w-full h-full bg-slate-200 pb-10'>
+             <div className='w-full h-[300px] md:h-[600px] shadow-md pt-20 '>
               <Image src={categoryData.mainImage} alt='Main Img' width={1000} height={700} priority className='w-full h-full  object-fill' />
              </div>
-             <div className='w-full pt-6 md:pt-8'>
-                 <h1 className={`font-bold md:text-4xl text-xl  text-[#00004f] text-center ${oswald.className} `}>{categoryData.title}</h1>
+             <div className='w-full pt-6 md:pt-8 '>
+                
+                 <h1 className={`font-bold md:text-4xl text-xl text-[#00004f] text-center ${oswald.className} `}>{categoryData.title}</h1>
+                 <div className='w-60 md:w-[400px] h-[1px] bg-[#f99b27] my-2 md:my-4 mx-auto'></div>
              </div>
              <div className='w-full h-auto py-6 px-6 md:px-10 '>
-                 <p className='text-center font-bold text-gray-950 text-[13px] leading-5 md:leading-8  md:text-lg '>{categoryData.description}</p>
+                 <p className='text-center  font-bold text-gray-950 text-[13px] leading-4 md:leading-8  md:text-lg '>{categoryData.description}</p>
              </div>
-             
+             <ItemList items={categoryData.items} />
+             </div>
+             <div className='w-full h-[1px] bg-[#00004f] my-2 md:my-4 mx-auto'></div>
+
+             <Footer />
         </div>
 
 
---
+++ src/components/otherComponents/ItemList.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React from 'react';
+import { Item } from '../../../serviceData';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+interface ItemsListProps{
+ items:Item[];
+}
+
+const ItemList : React.FC<ItemsListProps> = ({items}) => {
+  return (
+    <div className='w-full h-full bg-slate-200 px-6'>
+             {
+                items.map((item , index)=>(
+                    <motion.div 
+                    key={index}
+                    initial={{opacity :0, y:75}}
+                    whileInView={{opacity : 1 , y : 0}}
+                    viewport={{once:false}}
+                    transition={{duration : 0.5 , delay : 0.25 }}
+                    className={`mt-12 flex flex-col gap-6 ${index % 2 === 1 ? " md:flex-row-reverse gap-12": " md:flex-row"}`}
+                    >
+                        <div className='space-y-2 w-full'>
+                         {/* <h2 className='text-white/70 text-7xl my-4'>{`0 ${index +1}`}</h2> */}
+                         <h2 className='md:text-4xl 2xl:text-6xl text-2xl font-bold text-[#f99b27]'>{item.title}</h2>
+                         <p className='md:text-lg text-gray-950 break-words font-semibold md:font-bold md:p-4 px-2 py-2'>{item.description}</p>
+                         <div className='w-64 h-[1px] bg-[#00004f] my-4'>
+                         </div>
+                        
+                        </div>
+                        <div className='flex justify-center items-center md:h-[300px] md:w-[500px] w-[310px] h-[270px] '>
+                            <Image width={600} height={300} src={item.image} alt={item.title} className='object-fill hover:scale-105 duration-500 delay-75 transition-all md:h-[300px] md:w-[500px] h-[270px] border rounded border-gray-400' />
+                        </div>
+
+                    </motion.div>
+                ))
+            }
+
+    </div>
+  )
+}
+
+export default ItemList
