702e6fbaeecb5bab534b1d85e8ff47f14ebe684f
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-11-21 16:13:18 -0800

    add tables

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
            <p className='text-base px-4 md:px-0 md:text-lg  2xl:text-2xl font-bold text-center  font-serif text-[#00004f] p-2 inline-flex items-center'>
                 Providing Real Estate Virtual Assistant Services since 2009 in  United States and other parts of the world.
             </p>
-            <div className='  w-full  font-bold text-center my-2 text-lg'>
+            <div className='  w-full  font-bold text-center  text-lg'>
                    <h1 className={`md:bg-gradient-to-r  from-gray-700 via-indigo-900 to-gray-700 text-[#f99b27]  md:bg-clip-text md:text-transparent p-4 ${poppins.className}`}>ARE YOU A BUSY ENTREPRENEUR IMPACTED BY<br/>
                     INFLATION & HIGH OPERATIONAL COSTS?</h1>
                 </div>
@@ -34,7 +34,7 @@ const Hero = () => {
             <div className='md:hidden  mx-auto overflow-clip   w-[350px]  h-[200px] rounded-md border-white border-2 shadow-md '>
            <video src='/bgvideo.mp4' width={500} height={300}  className='md:hidden  rounded-md border-blue-400 object-cover' autoPlay loop   preload='autoPlay' muted />
          </div>
-           <div className='md:absolute mt-2 z-20 md:bottom-[100px]  right-0 w-full h-[200px] flex-col place-items-center   '>
+           <div className='md:absolute mt-4 z-20 md:bottom-[100px]  right-0 w-full h-[200px] flex-col place-items-center   '>
            <Herocard  />
            </div>
          </div>
